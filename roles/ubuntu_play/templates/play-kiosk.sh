3c67cad9c29f3d2ca5ab7bba2f1bc975b1e72f58
pukkamustard <pukkamustard@posteo.net>
2017-09-30 15:50:47 +0200

    Chromium hacks

--- roles/ubuntu_play/templates/play-kiosk.sh
+++ roles/ubuntu_play/templates/play-kiosk.sh
@@ -8,7 +8,14 @@ gsettings set org.gnome.desktop.screensaver logout-enabled false
 gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
 gsettings set org.gnome.desktop.session idle-delay 0
 
+# force chromium to think it exited cleanly
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' ~/.config/chromium/Default/Preferences
+
+# start chromium in kiosk mode...for ever. ever. ever, ever ever?
 while true
 do
   chromium-browser --disable-infobars --kiosk https://play.dividat.com/
 done
+
+# I'm sorry miss Jackson. I am for real...
