c42cf75505f6b8b3f0e312deddf8c5ef5fc1a568
pukkamustard <pukkamustard@posteo.net>
2017-09-30 15:25:41 +0200

    Disable power saving, screen saver and dimming

--- roles/ubuntu_play/templates/play-kiosk.sh
+++ roles/ubuntu_play/templates/play-kiosk.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+# disable power saving, screensaver and screen dimming
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.screensaver logout-enabled false
+gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
+gsettings set org.gnome.desktop.session idle-delay 0
+
 while true
 do
   chromium-browser --disable-infobars --kiosk https://play.dividat.com/
